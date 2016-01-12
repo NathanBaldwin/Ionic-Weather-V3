76f6f0c3260878b36845186bfeb7d93f25174ecb
NathanBaldwin <nathansbaldwin@gmail.com>
2016-01-12 11:02:44 -0600

    testing username

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'angular-skycons'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,37 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.controller('weatherCtrl', function($http) {
+  this.temp = "-";
+  this.iconName = "";
+  this.color = "blue";
+  // this = new weatherCtrl()
+
+
+
+  navigator.geolocation.getCurrentPosition(function(geopos) {
+    // this = {a: 'b'}
+    console.log(geopos);
+
+    var lat = geopos.coords.latitude;
+    var long = geopos.coords.longitude;
+    var apiKey = "255f1f7bd7cf37d67d37f0d8313cc27e";
+
+    var url = '/api/forecast/' + apiKey + '/' + lat + ',' + long;
+
+    $http.get(url).then(function (res) {
+      console.log("resolve object", res);
+      console.log("temperature", res.data.currently.temperature);
+
+      this.temp = res.data.currently.temperature;
+      this.iconName = res.data.currently.icon;
+      console.log("this.iconName", this.iconName);
+
+
+    }.bind(this))
+
+  }.bind(this));
+
+
+});
